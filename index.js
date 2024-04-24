9ea374fb5fb73f086f73a69772134ab7efcc629b
Inno1280 <75933454+Inno1280@users.noreply.github.com>
2024-04-24 21:39:45 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ var hamburgerEvent = (navigation, close, open) => {
     navigationItems.style.display = navigation;
     closeHam.style.display = close;
     openHam.style.display = open;
-};
-
-openHam.addEventListener("click", () => hamburgerEvent("flex", "block", "none"));
-closeHam.addEventListener("click", () => hamburgerEvent("none", "none", "block"));
+}; 
 
 openHam.addEventListener("click" , function(event){
+    event.preventDefault();
     hamburgerEvent("flex" , "block" , "none");
     console.log("I just got clicked")
 });
 closeHam.addEventListener("click" , function(event){
+    event.preventDefault(); 
     hamburgerEvent("none" , "none" , "block");
 });
